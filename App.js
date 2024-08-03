a97094f206540bb0bf42dc1e61c0c61aab313fe1
Al Ameen <sayedalameen1981@gmail.com>
2024-08-03 13:58:57 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * <div id = "root">
+ *   <div id ="parent">
+ *     <div id = "child1">
+ *        <h1>I'm h1 tag</h1>
+ *        <h2>I'm h1 tag</h2>
+ *       
+ * 
+ *      </div>
+ *     <div id = "child2">
+ *        <h1>I'm h1 tag</h1>
+ *        <h2>I'm h1 tag</h2>
+ *       
+ * 
+ *      </div>
+ *   </div>
+ * </div>
+ * 
+ */
+
+// const heading = React.createElement('div', { id:"parent" },[React.createElement('div',{id:"child1"},
+//     [React.createElement('h1',{},"I'm h1 tag"),React.createElement('h2',{},"I'm h2 tag")]),
+//     {id:"child2"},[React.createElement('h3',{},"I'm h3 tag"),React.createElement('h4',{},"I'm h4 tag")]] )) 
+
+ 
+//         const root = ReactDOM.createRoot(document.querySelector('#root'))
+//         root.render(heading)         
